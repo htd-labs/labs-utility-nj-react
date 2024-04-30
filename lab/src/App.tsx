6ce72563b1a5eb4b99c067ce9ac0f67e788e147e
JonathanT.Cantu <130400648+Jonah314@users.noreply.github.com>
2024-04-30 15:33:22 -0500

    saving lab progress: 04-30 15:33

---
+++ lab/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ParentComponent from './components/parent-component/parent-component';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent></ParentComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab/src/components/class-child-component/class-child-component.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+/* This TypeScript interface allows us to model the props' attributes to enforce compile time safety. The attributes
+of a prop can be any combination of datatypes. Here is an example below.*/
+interface MyProps {
+    myNumProp: number;
+    myStringProp: string;
+}
+
+
+/* Class components have a props object inherently. You need only define the datatypes by providing an interface in the diamond brackets (generics). From this
+point you can access the props object by using "this.props".*/
+class ClassChildComponent extends React.Component<MyProps>{
+
+
+    render() {
+
+        return (
+            <React.Fragment>
+                <h4>Class Child Display</h4>
+
+                {/* Here is an example of you display props data. */}
+                <p>Here is the "myStringProp" prop data: {this.props.myStringProp}</p>
+
+                <div className="todo-container">
+                    {/* TODO: alter the text below so that it properly displays the "myNumProp" data, similar to the text above. */}
+                    <p>Here is the "myNumProp" prop data: {this.props.myStringProp}</p>
+                    <p></p>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ClassChildComponent;
